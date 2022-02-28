49e08ab60feb4edbad8543994026ca7327eb2904
Shruthi S <82823305+Shruthi-Sivagnanam@users.noreply.github.com>
2022-02-28 19:27:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(path.join(__dirname, "client", "build")));
 
 const PORT = process.env.PORT || 5000;
 db();
@@ -31,7 +30,10 @@ app.get("/", (req, res) => {
 app.use("/user", router_user);
 app.use("/blog", route_blog);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build/"));
+  app.get("/*", (req, res) => {
+    res.send(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 app.listen(PORT);

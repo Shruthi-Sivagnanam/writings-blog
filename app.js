2ff9d7dedf1655516f3469967d550b13c9217095
Shruthi S <82823305+Shruthi-Sivagnanam@users.noreply.github.com>
2022-02-28 21:16:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,10 +28,12 @@ app.listen(PORT);
 app.use("/user", router_user);
 app.use("/blog", route_blog);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build/"));
-  app.get("*", (req, res) => {
-    res.send(path.join(__dirname, "client", "build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
